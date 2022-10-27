0efd2c9739a31c45010b2e6995b3886cd6d65513
m_wallet_app <mwallet.apps@gmail.com>
2022-10-27 09:56:44 +0200

    Flow producer.

---
+++ app/src/main/java/mo/atef/codingwithnerds/coroutines/FlowActivityImpl.kt
@@ -0,0 +1,19 @@
+package mo.atef.codingwithnerds.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.flow.flow
+
+class FlowActivityImpl : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flow_impl)
+
+        // Producer
+        flow<Int> {
+            for (i in 1..100){
+                emit(i)
+            }
+        }
+    }
+}
